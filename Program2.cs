c6814c756c47cc3efd61d105dd5c4b3f64fc63ba
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-20 19:16:40 +0200

    Четверте завдання  з практичної роботи

--- Program2.cs
+++ Program2.cs
@@ -6,33 +6,28 @@ using System.Threading.Tasks;
 
 namespace _20._03
 {
-    using System;
-
-    namespace ArithmeticOperationsApp
+    class Program2
     {
-        class Program2
+        public delegate int ArithmeticOperation(int a, int b);
+
+        static void Main(string[] args)
         {
-            public delegate int ArithmeticOperation(int a, int b);
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            ArithmeticOperation add = (a, b) => a + b;
+            ArithmeticOperation subtract = (a, b) => a - b;
+            ArithmeticOperation multiply = (a, b) => a * b;
 
-            static void Main(string[] args)
-            {
-                Console.InputEncoding = Encoding.Unicode;
-                Console.OutputEncoding = Encoding.Unicode;
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.CursorVisible = false;
-                ArithmeticOperation add = (a, b) => a + b;
-                ArithmeticOperation subtract = (a, b) => a - b;
-                ArithmeticOperation multiply = (a, b) => a * b;
+            Console.WriteLine($"Додавання: {PerformOperation(add, 5, 3)}");
+            Console.WriteLine($"Віднімання: {PerformOperation(subtract, 5, 3)}");
+            Console.WriteLine($"Множення: {PerformOperation(multiply, 5, 3)}");
+        }
 
-                Console.WriteLine($"Додавання: {PerformOperation(add, 5, 3)}");
-                Console.WriteLine($"Віднімання: {PerformOperation(subtract, 5, 3)}");
-                Console.WriteLine($"Множення: {PerformOperation(multiply, 5, 3)}");
-            }
-
-            static int PerformOperation(ArithmeticOperation operation, int a, int b)
-            {
-                return operation(a, b);
-            }
+        static int PerformOperation(ArithmeticOperation operation, int a, int b)
+        {
+            return operation(a, b);
         }
     }
 
