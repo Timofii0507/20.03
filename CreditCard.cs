c94ce27da1ae7d57cf080097fdccd24d2a62820a
Timofii0507 <timofiiyurkov@gamil.com>
2024-03-20 20:34:05 +0200

    Третє завдання з дз

---
+++ CreditCard.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._03
+{
+    public class PinChangedEventArgs : EventArgs
+    {
+        public string OldPin { get; }
+        public string NewPin { get; }
+
+        public PinChangedEventArgs(string oldPin, string newPin)
+        {
+            OldPin = oldPin;
+            NewPin = newPin;
+        }
+    }
+
+    public class CreditCard
+    {
+        public string CardNumber { get; }
+        public string CardHolderName { get; }
+        public DateTime ExpiryDate { get; }
+        private string _pin;
+        public double CreditLimit { get; }
+        private double _balance;
+
+        public event EventHandler<PinChangedEventArgs> PinChanged;
+
+        public CreditCard(string cardNumber, string cardHolderName, DateTime expiryDate, string pin, double creditLimit)
+        {
+            CardNumber = cardNumber;
+            CardHolderName = cardHolderName;
+            ExpiryDate = expiryDate;
+            _pin = pin;
+            CreditLimit = creditLimit;
+        }
+
+        public void TopUp(double amount)
+        {
+            _balance += amount;
+        }
+
+        public void Spend(double amount)
+        {
+            if (_balance + amount > CreditLimit)
+            {
+                Console.WriteLine("Ви перевищили кредитний ліміт.");
+                return;
+            }
+
+            _balance -= amount;
+        }
+
+        public void StartUsingCredit()
+        {
+            Console.WriteLine("Початок використання кредитних коштів.");
+        }
+
+        public bool IsLimitReached(double amount)
+        {
+            return _balance + amount > CreditLimit;
+        }
+
+        public void ChangePin(string newPin)
+        {
+            string oldPin = _pin;
+            _pin = newPin;
+            OnPinChanged(oldPin, newPin);
+        }
+
+        protected virtual void OnPinChanged(string oldPin, string newPin)
+        {
+            PinChanged?.Invoke(this, new PinChangedEventArgs(oldPin, newPin));
+        }
+    }
+}
---
+++ Program7.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.CursorVisible = false;
+            Console.WriteLine("Введіть номер картки:");
+            string cardNumber = Console.ReadLine();
+
+            Console.WriteLine("Введіть ПІБ власника:");
+            string cardHolderName = Console.ReadLine();
+
+            Console.WriteLine("Введіть термін дії карти (MM/yyyy):");
+            DateTime expiryDate;
+            while (!DateTime.TryParseExact(Console.ReadLine(), "MM/yyyy", null, System.Globalization.DateTimeStyles.None, out expiryDate))
+            {
+                Console.WriteLine("Некоректний формат. Будь ласка, введіть термін дії карти у форматі MM/yyyy:");
+            }
+
+            Console.WriteLine("Введіть PIN:");
+            string pin = Console.ReadLine();
+
+            Console.WriteLine("Введіть кредитний ліміт:");
+            double creditLimit;
+            while (!double.TryParse(Console.ReadLine(), out creditLimit) || creditLimit <= 0)
+            {
+                Console.WriteLine("Некоректне значення. Будь ласка, введіть кредитний ліміт (позитивне число):");
+            }
+
+            CreditCard creditCard = new CreditCard(cardNumber, cardHolderName, expiryDate, pin, creditLimit);
+
+            creditCard.PinChanged += (sender, e) =>
+            {
+                Console.WriteLine($"PIN змінено з {e.OldPin} на {e.NewPin}");
+            };
+
+            Console.WriteLine("Введіть суму для поповнення рахунку:");
+            double topUpAmount;
+            while (!double.TryParse(Console.ReadLine(), out topUpAmount) || topUpAmount <= 0)
+            {
+                Console.WriteLine("Некоректне значення. Будь ласка, введіть суму для поповнення рахунку (позитивне число):");
+            }
+            creditCard.TopUp(topUpAmount);
+
+            Console.WriteLine("Введіть суму для витрати з рахунку:");
+            double spendAmount;
+            while (!double.TryParse(Console.ReadLine(), out spendAmount) || spendAmount <= 0)
+            {
+                Console.WriteLine("Некоректне значення. Будь ласка, введіть суму для витрати з рахунку (позитивне число):");
+            }
+            creditCard.Spend(spendAmount);
+
+            creditCard.StartUsingCredit();
+
+            Console.WriteLine($"Досягнуто ліміту: {creditCard.IsLimitReached(4500)}");
+
+            Console.WriteLine("Введіть новий PIN:");
+            string newPin = Console.ReadLine();
+            creditCard.ChangePin(newPin);
+
+            Console.ReadLine();
+        }
+    }
+}
